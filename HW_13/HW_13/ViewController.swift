cdf3ad2ac3c8be47d77ff4586a08a10422077f13
Sergievich <segievich.air@gmail.com>
2021-06-27 19:38:16 +0300

    FirstOpen

---
+++ HW_13/HW_13/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW_13
+//
+//  Created by Artiom on 27.06.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
